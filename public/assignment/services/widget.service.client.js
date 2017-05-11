f20d3548e150e0007451ef0a7ac76ecb1112c55b
Babubhai <shubhsharma10@gmail.com>
2017-05-11 13:47:26 -0400

    FInishing Assignment 3

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -7,11 +7,11 @@
     function WidgetService() {
         var widgets =
             [
-                {_id: "123", widgetType: "HEADING",     pageId: "875" , size:2,         text: "GIZMODO"},
-                {_id: "234", widgetType: "HEADING",     pageId: "875" , size:4,         text: "Lorem ipsum"},
+                {_id: "123", widgetType: "HEADER",     pageId: "875" , size:2,         text: "GIZMODO"},
+                {_id: "234", widgetType: "HEADER",     pageId: "875" , size:4,         text: "Lorem ipsum"},
                 {_id: "345", widgetType: "IMAGE",       pageId: "875" , width:"25%",   url: "http://lorempixel.com/1600/900/people/"},
                 {_id: "456", widgetType: "HTML",        pageId: "875" ,                 text: "<p> Lorem ipsum</p>"},
-                {_id: "567", widgetType: "HEADING",     pageId: "983" , size:4,         text: "GIZMODO"},
+                {_id: "567", widgetType: "HEADER",     pageId: "983" , size:4,         text: "GIZMODO"},
                 {_id: "678", widgetType: "YOUTUBE",     pageId: "983" , width:"25%",   url: "https://youtu.be/nO6Sy6rx49k"},
                 {_id: "789", widgetType: "HTML",        pageId: "983" ,                 text: "<p> Lorem ipsum</p>"}
             ];
